938573c8dd938a09804fc6941f326340c6bc120e
cmdel <cmdelzell@gmail.com>
2024-03-04 12:38:37 -0600

    fixing merge issues

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
-import "./index.scss";
 import App from "./App";
 import { UserProvider } from "./contexts/user.context";
 import { ProductsProvider } from "./contexts/products.context";
 import { CartProvider } from "./contexts/cart.context";
-//import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+import "./index.scss";
+
+const rootElement = document.getElementById("root");
+
+render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
@@ -21,10 +23,5 @@ ReactDOM.render(
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root")
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,12 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 
-import CartIcon from "../../components/cart-icon/cart-icon.component";
-import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
-
-import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { UserContext } from "../../contexts/user.context";
 import { CartContext } from "../../contexts/cart.context";
+
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
@@ -20,24 +20,20 @@ const Navigation = () => {
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          <div>
-            <CrwnLogo className="logo" />
-          </div>
+          <CrwnLogo className="logo" />
         </Link>
         <div className="nav-links-container">
-          <Link to="/" className="nav-link">
-            Home
+          <Link className="nav-link" to="/shop">
+            SHOP
           </Link>
-          <Link to="/shop" className="nav-link">
-            Shop
-          </Link>
+
           {currentUser ? (
             <span className="nav-link" onClick={signOutUser}>
-              Sign-Out
+              SIGN OUT
             </span>
           ) : (
-            <Link to="/signIn" className="nav-link">
-              Sign-In
+            <Link className="nav-link" to="/auth">
+              SIGN IN
             </Link>
           )}
           <CartIcon />
